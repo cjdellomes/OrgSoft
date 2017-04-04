5399b6c35239a4a8235300d2c3a4672a7aac609a
Chris Dellomes <cjdellomes@gmail.com>
2017-04-03 17:57:14 -0700

    Working on review dashboard datatable

---
+++ static/js/reviewdash.js
@@ -0,0 +1,45 @@
+$(function (event) {
+	
+	var table = $('#Table').DataTable({
+		initComplete: function (settings, json) {
+		    $("#Table").show();
+		},
+		paging: false,
+		dom: "Bfrtip",
+		buttons: ['copy', 'excel', 'pdf', 'csv', 'print']
+	});
+
+	// On column text box change, search input and redraw datatable
+	table.columns().eq(0).each(function (colIdx) {
+	    $("input", table.column(colIdx).header()).on("keyup change", function () {
+	        table.column(colIdx)
+	            .search(this.value)
+	            .draw();
+	    });
+
+	    // When clicking the search box, the sort won't occur
+	    $("input", table.column(colIdx).header()).on("click", function (e) {
+	        e.stopPropagation();
+	    });
+	});
+
+	// Move datatables search to the panel header
+	$('.dataTables_filter').appendTo('#dt-search');
+
+	// Move datatables export buttons to the panel header
+	$('.dt-buttons').appendTo('#dt-buttons');
+	$('.buttons-html5').hide();
+	$('.buttons-print').hide();
+
+	// Show export button based on dropdown selection
+	$('#export').change(function () {
+	    var option = $(this).val();
+	    $('.buttons-html5').hide();
+	    $('.buttons-print').hide();
+	    if (option != "") {
+	        var button = '.buttons-' + option;
+	        $(button).show();
+	    }
+	});
+
+});
