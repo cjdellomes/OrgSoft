c40d6e8181c2eff66b1ae009efc9f2857ead8d21
Chris Dellomes <cjdellomes@gmail.com>
2017-04-03 18:24:40 -0700

    Formatted review dashboard datatable

--- static/js/reviewdash.js
+++ static/js/reviewdash.js
@@ -1,45 +1,50 @@
 $(function (event) {
-	
-	var table = $('#Table').DataTable({
-		initComplete: function (settings, json) {
-		    $("#Table").show();
-		},
-		paging: false,
-		dom: "Bfrtip",
-		buttons: ['copy', 'excel', 'pdf', 'csv', 'print']
-	});
 
-	// On column text box change, search input and redraw datatable
-	table.columns().eq(0).each(function (colIdx) {
-	    $("input", table.column(colIdx).header()).on("keyup change", function () {
-	        table.column(colIdx)
-	            .search(this.value)
-	            .draw();
-	    });
+	var initiateDataTable = function () {
 
-	    // When clicking the search box, the sort won't occur
-	    $("input", table.column(colIdx).header()).on("click", function (e) {
-	        e.stopPropagation();
-	    });
-	});
+		var table = $('#Table').DataTable({
+			initComplete: function (settings, json) {
+			    $("#Table").show();
+			},
+			paging: false,
+			dom: "Bfrtip",
+			buttons: ['copy', 'excel', 'pdf', 'csv', 'print']
+		});
 
-	// Move datatables search to the panel header
-	$('.dataTables_filter').appendTo('#dt-search');
+		// On column text box change, search input and redraw datatable
+		table.columns().eq(0).each(function (colIdx) {
+		    $("input", table.column(colIdx).header()).on("keyup change", function () {
+		        table.column(colIdx)
+		            .search(this.value)
+		            .draw();
+		    });
 
-	// Move datatables export buttons to the panel header
-	$('.dt-buttons').appendTo('#dt-buttons');
-	$('.buttons-html5').hide();
-	$('.buttons-print').hide();
+		    // When clicking the search box, the sort won't occur
+		    $("input", table.column(colIdx).header()).on("click", function (e) {
+		        e.stopPropagation();
+		    });
+		});
 
-	// Show export button based on dropdown selection
-	$('#export').change(function () {
-	    var option = $(this).val();
-	    $('.buttons-html5').hide();
-	    $('.buttons-print').hide();
-	    if (option != "") {
-	        var button = '.buttons-' + option;
-	        $(button).show();
-	    }
-	});
+		// Move datatables search to the panel header
+		$('.dataTables_filter').appendTo('#dt-search');
+
+		// Move datatables export buttons to the panel header
+		$('.dt-buttons').appendTo('#dt-buttons');
+		$('.buttons-html5').hide();
+		$('.buttons-print').hide();
+
+		// Show export button based on dropdown selection
+		$('#export').change(function () {
+		    var option = $(this).val();
+		    $('.buttons-html5').hide();
+		    $('.buttons-print').hide();
+		    if (option != "") {
+		        var button = '.buttons-' + option;
+		        $(button).show();
+		    }
+		});
+	}
+
+	initiateDataTable();
 
 });
